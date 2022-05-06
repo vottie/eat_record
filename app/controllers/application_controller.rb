df3d1469c9449d1dddba3376649830b8e16ed7d2
Tsuboi Masafumi <tsuboi23@gmail.com>
2022-05-06 16:03:31 +0900

    Add sign out path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resources)
     eat_records_path
   end
+
+  def after_sign_out_path_for(resources)
+    new_user_session_path
+  end
 end
