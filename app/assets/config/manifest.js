48211c1cc0cb2c9330b6518030828640fe36c012
Tsuboi Masafumi <tsuboi23@gmail.com>
2022-05-07 10:52:36 +0900

    Add manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,17 @@
 //= link_tree ../../javascript .js
 //= link_directory ../../../vendor/javascript .js
 //= link_tree ../builds
+{
+  "lang": "ja",
+  "name": "eat records",
+  "short_name": "eat records",
+  "start_url": "https://boltech21.net/eat_records/users/sign_in",
+  "display": "standalone",
+  "theme_color": "#0971ac",
+  "icons": [{
+      "src": "/images/eat-icon.png",
+    "sizes": "128x128",
+    "type": "image/png"
+  }]
+}
+
---
+++ app/javascript/serviceWorker.js
@@ -0,0 +1,12 @@
+<script>
+window.addEventListener('load', function() {
+ if ('serviceWorker' in navigator) {
+ navigator.serviceWorker.register("/serviceWorker.js")
+ .then(function(registration) {
+ console.log("serviceWorker registed.");
+ }).catch(function(error) {
+ console.warn("serviceWorker error.", error);
+ });
+ }
+});
+</script>
