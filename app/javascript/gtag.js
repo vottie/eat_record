5b4ff17ecef65113e1376fbe6ca11dfafc22c7ee
Tsuboi Masafumi <tsuboi23@gmail.com>
2022-05-15 16:11:25 +0900

    Fix google analytics.

--- app/javascript/gtag.js
+++ app/javascript/gtag.js
@@ -1,5 +1,4 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0KG55FDX3Z"></script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
