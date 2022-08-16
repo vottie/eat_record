e41a1ad6952aeba4e0187f2b989a37e11f1b82de
Tsuboi Masafumi <tsuboi23@gmail.com>
2022-08-16 10:52:56 +0900

    Add data to shops.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Shop.create(
+  name: 'KFC',
+  place: '子母口店'
+)
+
+Shop.create(
+  name: 'マクドナルド',
+  place: '元住吉店'
+)
+Shop.create(
+  name: 'ドミノピザ',
+  place: '下小田中店'
+)
+
+Shop.create(
+  name: 'リップル',
+  place: ''
+)
+
+Shop.create(
+  name: '麺小屋てち',
+  place: ''
+)
